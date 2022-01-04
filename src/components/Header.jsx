deceeea5f3b1986228e6e15f5ca80b27c90e00b9
Artsem Samonau <artemgomel89@gmail.com>
2022-01-04 20:58:16 +0300

    init: start react project

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+   return <header className='header'>
+        <img
+            src='https://lh3.googleusercontent.com/proxy/MIxOq9LJMnjepOauNFS21UIP2dKMTOBs-iwpJRyu2GGQPxoXt4lSvrrotrzBtxekihsKowNzVonZQlmGer0l4jPLzPoTuRG8tSGChuVBBREaMzw0vGvrbA'
+            alt='main-logo'
+        />
+    </header>
+}
+
+export default Header;
