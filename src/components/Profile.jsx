6d484d437a82fbcd9aec268497f1f9a6903c1dc6
Artsem Samonau <artemgomel89@gmail.com>
2022-01-04 20:58:44 +0300

    feat: add components

---
+++ src/components/Profile.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+ const Profile = () => {
+     return <div className='profile'>
+         <div>
+             <img
+                 src='https://i.pinimg.com/550x/56/e5/3b/56e53bd754deedf4254a8172e0f7d580.jpg'
+                 alt='dog' className='dog'
+             />
+             <div> ava + description</div>
+             <div>
+                 My posts
+                 <div>new post</div>
+                 <div>post1</div>
+                 <div>post1</div>
+             </div>
+         </div>
+     </div>
+ }
+
+ export default Profile;
